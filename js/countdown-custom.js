387ae60c19bdb6c1345fda95539b3871a76e1cf9
Zuhri Saifudin <rizalswag70@gmail.com>
2020-11-12 11:41:20 +0700

    first commit

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        jQuery(function () {
+            jQuery('#defaultCountdown').countdown({until: new Date(2020, 12, 12, 10)}); // year, month, date, hour
+        });
+});		
+
