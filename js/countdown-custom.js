257923c04d1eca0635aa0b7a251a4dc2f4804f14
Zuhri Saifudin <rizalswag70@gmail.com>
2020-11-16 02:13:01 +0700

    revisi 1

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function() {
         jQuery(function () {
-            jQuery('#defaultCountdown').countdown({until: new Date(2020, 12, 12, 10)}); // year, month, date, hour
+        	jQuery('#defaultCountdown').countdown({until: new Date(2020, 11, 12, 10)});
+            
         });
 });		
 
